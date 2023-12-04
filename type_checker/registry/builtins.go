cceb6ac752802a4806e6aa09dd1379964aa71719
GearsDatapacks <surya@seriousinfinitude.com>
2023-12-04 17:10:24 +0000

    fix: make parse functions return error types

--- type_checker/registry/builtins.go
+++ type_checker/registry/builtins.go
@@ -20,13 +20,17 @@ func registerBuiltin(name string, parameters params, returnType types.ValidType)
 	Builtins[name] = data
 }
 
+func err(ty types.ValidType) types.ValidType {
+	return &types.ErrorType{ResultType: ty}
+}
+
 func registerBuiltins() {
 	registerBuiltin("print", params{&types.Any{}}, &types.Void{})
 	registerBuiltin("printil", params{&types.Any{}}, &types.Void{})
 	registerBuiltin("prompt", params{stringType}, stringType)
 	registerBuiltin("toString", params{&types.Any{}}, stringType)
-	registerBuiltin("parseInt", params{stringType}, intType)
-	registerBuiltin("parseFloat", params{stringType}, floatType)
-	registerBuiltin("readFile", params{stringType}, &types.ErrorType{ResultType: stringType})
-	registerBuiltin("writeFile", params{stringType, stringType}, &types.ErrorType{ResultType: &types.Void{}})
+	registerBuiltin("parseInt", params{stringType}, err(intType))
+	registerBuiltin("parseFloat", params{stringType}, err(floatType))
+	registerBuiltin("readFile", params{stringType}, err(stringType))
+	registerBuiltin("writeFile", params{stringType, stringType}, err(&types.Void{}))
 }
